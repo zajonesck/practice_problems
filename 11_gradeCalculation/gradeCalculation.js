a372b203cbff7c0fb3b4125c85eb83d60f8d360a
Zack Jones <zajonesck83@gmail.com>
2023-08-29 23:04:50 -0500

    grade calculation

---
+++ 11_gradeCalculation/gradeCalculation.js
@@ -0,0 +1,16 @@
+let gradeCalculation = function (score, absences) {
+  if (absences >= 6) {
+    return "F";
+  }
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+};
